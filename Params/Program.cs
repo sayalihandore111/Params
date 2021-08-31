4d41e5fa69a3863b14304384a4ddc803e4883b15
sayalihandore111 <sayalihandore111@gmail.com>
2021-08-31 22:46:17 +0530

    Add project files.

---
+++ Params/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Params
+{
+    class Geeks
+    {
+
+        public void result(params object[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Geeks gfg = new Geeks();
+            gfg.result("Geeks", "GFG","ProGeek Cup 2.0", "G4G", "100");
+        }
+    }
+}
